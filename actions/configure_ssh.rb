1c315857e26962f13455db58c0ae7b94325ec1e9
Beau Barker <rtecoder@gmail.com>
2016-10-10 19:09:30 -0400

    Fix permission issue with SSH config.

--- actions/configure_ssh.rb
+++ actions/configure_ssh.rb
@@ -3,12 +3,13 @@ module BarkestServerPrep
 
     def configure_ssh(shell)
 
+
       pa_rex = /#\s*PubkeyAuthentication\s+[^\n]*\n/
       rl_rex = /#\s*PermitRootLogin\s+[^\n]*\n/
 
       admin_home = shell.exec("eval echo \"~#{admin_user}\"").split("\n").first.strip
 
-      shell.sudo_exec 'cp -f /etc/ssh/sshd_config ~/tmp_sshd_conf'
+      shell.sudo_exec "cp -f /etc/ssh/sshd_config #{admin_home}/tmp_sshd_conf && chown #{admin_user}:#{admin_user} #{admin_home}/tmp_sshd_conf"
 
       contents = shell.read_file "#{admin_home}/tmp_sshd_conf"
       contents = contents
@@ -16,9 +17,9 @@ module BarkestServerPrep
                      .gsub(rl_rex, "PermitRootLogin no\n")        # disable root login
 
       shell.write_file "#{admin_home}/tmp_sshd_conf", contents
-      shell.sudo_exec 'chown root:root ~/tmp_sshd_conf'
-      shell.sudo_exec 'chmod 600 ~/tmp_sshd_conf'
-      shell.sudo_exec 'mv -f ~/tmp_sshd_conf /etc/ssh/sshd_config'
+      shell.sudo_exec "chown root:root #{admin_home}/tmp_sshd_conf"
+      shell.sudo_exec "chmod 600 #{admin_home}/tmp_sshd_conf"
+      shell.sudo_exec "mv -f #{admin_home}/tmp_sshd_conf /etc/ssh/sshd_config"
 
       # shouldn't disconnect us ... but who knows...
       shell.sudo_exec 'systemctl restart sshd.service' rescue nil
--- actions/ssh_copy_id.rb
+++ actions/ssh_copy_id.rb
@@ -2,6 +2,7 @@ module BarkestServerPrep
   module SshCopyId
 
     def ssh_copy_id(shell)
+      stat = shell.instance_variable_get(:@stat_console)
 
       if File.exist?('~/.ssh/rsa_id.pub')
         data = File.read('~/.ssh/rsa_id.pub')
@@ -14,9 +15,9 @@ module BarkestServerPrep
         shell.write_file "#{deploy_home}/authorized_keys", data
         shell.exec 'chmod 600 ~/.ssh/authorized_keys'
 
-        @status_console.append_data "\033[0;32;1mDone.\033[0m Key based authentication should be enabled.\n"
+        stat.append_data "\033[0;32;1mDone.\033[0m Public key setup.\n"
       else
-        @status_console.append_data "\033[0;31mSkipped!\033[0m No rsa id for current user.\n"
+        stat.append_data "\033[0;31mSkipped!\033[0m No rsa id for current user.\n"
       end
 
     end
